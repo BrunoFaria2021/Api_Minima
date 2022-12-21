5caf6b6b95b115db9fe7f20c07c18224f8fa0df7
BrunoFaria2021 <bmc_ti@outlook.com>
2022-12-21 18:03:47 -0300

    CRUD BASE

--- ApiMinima/ApiMinima/Program.cs
+++ ApiMinima/ApiMinima/Program.cs
@@ -1,15 +1,80 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Serviço de injeção de dependecia
+
 builder.Services.AddEndpointsApiExplorer();
-object value = builder.Services.();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("Cliente"));
 
 var app = builder.Build();
-//Tratamento de erro.
+
+//Tratamento de erro do desenvolvedor.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+
+//endpoint
+
+app.MapGet("/Clientes", async(AppDbContext dbContext )=>
+        await dbContext.Clientes.ToListAsync());
+
+app.MapGet("/Clientes/{id}", async (int id, AppDbContext dbContext) =>
+        await dbContext.Clientes.FirstOrDefaultAsync(a => a.Id == id));
+
+app.MapPost("/Clientes", async (Cliente cliente, AppDbContext dbContext) =>
+{
+    dbContext.Clientes.Add(cliente);
+    await dbContext.SaveChangesAsync();
+
+    return cliente;
+});
+
+app.MapPut("/Clientes/{id}", async(int id, Cliente cliente,
+     AppDbContext dbContext) =>
+{
+    dbContext.Entry(cliente).State = EntityState.Modified;
+    await dbContext.SaveChangesAsync();
+
+    return cliente;
+});
+
+app.MapDelete("/Clientes/{id}", async (int id,AppDbContext dbContext) =>
+{
+    var cliente = await dbContext.Clientes.FirstOrDefaultAsync(a => a.Id == id);
+
+    if (cliente != null)
+    {
+        dbContext.Clientes.Remove(cliente);
+        await dbContext.SaveChangesAsync();
+    }
+    return;
+});
+
+
+app.UseSwaggerUI();
 
 app.Run();
+
+//Record são tipo de referencia que usa a igualdade baseada em valores
+
+public class Cliente
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Cliente> Clientes { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">:file_cabinet: MinimalAPIs README.md</h1>
+
+## :memo: Descrição
+
+Essa aplicação foi elaborada com intuito de ver sobre ApisMinimal se trata apenas de um demo então ficou algo bem basico onde tratamo um erro e fizemos algumas injeção de dependencia se trata apenas de um CRUD BASE(Demo).
+  APIs mínimas são arquitetadas para criar APIs HTTP com dependências mínimas. Eles são ideais para microsserviços e aplicativos que desejam incluir apenas os arquivos mínimos, recursos e dependências em ASP.NET Core.
+
+
+## :books: Funcionalidades
+
+Objetivo do WebApplication configurar de forma simples e objetiva um app web ele simplifica os recurso juntando tudo em um lugar só.
+
+## ✔️ :wrench: Tecnologias / Metodologias utilizadas
+ 
+* CSharp
+  
+* AspNetCore
+  
+* Injeções de depencências
+   
+* Microsoft.EntityFrameworkCore.InMemory
+  
+* template asp.net core empty
+  
+* Swashbuckle.AspNetCore.Swagger
+
+## :rocket: Rodando o projeto
+
+Para rodar o repositório é necessário clonar o mesmo, e iniciar a aplicação(se trata apenas de uma demo).
+
+
+## :handshake: Colaboradores
+https://github.com/BrunoFaria2021
+
+## :dart: Status do projeto
+<p align="
+LEFT
+">
+<img src="http://img.shields.io/static/v1?label=STATUS&message=Em desenvolvimento &color=GREEN&style=for-the-badge"/>
+</p>
+
